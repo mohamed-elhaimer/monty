bc6681677acf587781afdb0177bd0c99e7b7debb
SimoLogiC <mohamedelheimer0@gmail.com>
2023-10-20 02:49:36 +0100

    monty.h

--- monty.h
+++ monty.h
@@ -18,9 +18,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -32,8 +32,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 /**
  * struct bus_s - variables -args, file, line content
